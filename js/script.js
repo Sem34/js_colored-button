c87070e75607760d5eb67ba99449b091150e5f4a
Sem34 <82157833+Sem34@users.noreply.github.com>
2022-09-06 22:12:28 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// const button = document.querySelector('.button');
+// const circle = document.querySelector('.circle')
+
+let color = 'green';
+
+function change(identifier) {
+    identifier.style.background = color;
+}
+
